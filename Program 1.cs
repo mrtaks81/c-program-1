afd6be90c23e358de085fe6d32550749f8aad67a
mrtaks81 <32458553+mrtaks81@users.noreply.github.com>
2017-10-02 19:53:59 +0300

    Add files via upload

---
+++ Program 1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, toplam;
+            Console.Write("ilk değeri girin");
+            a = Convert.ToInt16(Console.ReadLine());
+            Console.Write("ilk değeri girin");
+            b = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("{0} ve {1} sayılarının toplamı=",a,b);
+           
+            toplam=a+b;
+            Console.WriteLine("toplam={0}"+(toplam));
+            Console.ReadKey();
+        }
+    }
+}
